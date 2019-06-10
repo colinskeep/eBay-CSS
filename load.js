90b7336cbd075959406890da5c649a1d4754b99b
Colin Inskeep <colinjinskeep@gmail.com>
2019-06-10 16:36:11 -0700

    test

--- load.js
+++ load.js
@@ -1,13 +1,7 @@
 $(document).ready(function(){
-	var source = $('#urlp').text() + "/make-an-offer";
-	console.log(source);
+	var source = $('#urlp').text() + "/make-an-offer #bstr";
 	var name = $('#ProductDetails h1').text();
-	console.log(name);
 	var url = window.location.href;
 	$('.modal-header h3').text("Make an Offer for "+name);
-    //$('.modal-body').load(source,
-        //function(){
-            //document.querySelector("#contact_question").value = `I would like to make an offer for "+name+"\n("+url+")`;
-            //console.log(`I would like to make an offer for "+name+"\n("+url+")`)
-        //});
+	$('.modal-body').load(source,function(){$('#contact_question').val("I would like to make an offer for "+name+"\n("+url+")");});
 })

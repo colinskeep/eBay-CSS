a541df859d68fe5c33115141a8ca0176a29e4190
Colin Inskeep <colinjinskeep@gmail.com>
2019-06-10 16:38:52 -0700

    test

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var source = $('#urlp').text() + "/make-an-offer #bstr";
+	var source = $('#urlp').text() + "/make-an-offer";
 	var name = $('#ProductDetails h1').text();
 	var url = window.location.href;
 	$('.modal-header h3').text("Make an Offer for "+name);

323ab65dac077bfd401338c823f18b603747a0af
Colin Inskeep <colinjinskeep@gmail.com>
2019-06-14 11:55:11 -0700

    remove log

--- load.js
+++ load.js
@@ -3,8 +3,5 @@ $(document).ready(function(){
 	var name = $('#ProductDetails h1').text();
 	var url = window.location.href;
 	$('.modal-header h3').text("Make an Offer for "+name);
-	//$('.modal-body').load(source);
-	console.log('555');
-	//$('.modal-body').load(source,function(){$('#contact_question').val("I would like to make an offer for "+name+"\n("+url+")");});
 	document.querySelector("#contact_question").value = 'Offer: \nFor: '+name
 })

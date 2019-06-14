8f364dc279da320be35a7318c27cb96ae2cb4520
Colin Inskeep <colinjinskeep@gmail.com>
2019-06-14 16:03:01 -0700

    update

--- load.js
+++ load.js
@@ -3,5 +3,5 @@ $(document).ready(function(){
 	var name = $('#ProductDetails h1').text();
 	var url = window.location.href;
 	$('.modal-header h3').text("Make an Offer for "+name);
-	document.querySelector("#contact_question").value = 'Offer: \nFor: '+name
+	document.querySelector("#contact_question").value = 'Offer: \n\n\n\nFor: '+name
 })
